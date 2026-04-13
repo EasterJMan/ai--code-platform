1116345175f2fde1eaa6de010532133144bbf022
EasterJMan <2568370392@qq.com>
2026-04-13 10:51:41 +0800

    langgraph工作流并发处理节点

--- src/main/java/com/jzy/aicodeplatform/langgraph4j/state/WorkflowContext.java
+++ src/main/java/com/jzy/aicodeplatform/langgraph4j/state/WorkflowContext.java
@@ -1,5 +1,6 @@
 package com.jzy.aicodeplatform.langgraph4j.state;
 
+import com.jzy.aicodeplatform.langgraph4j.model.ImageCollectionPlan;
 import com.jzy.aicodeplatform.langgraph4j.model.ImageResource;
 import com.jzy.aicodeplatform.langgraph4j.model.QualityResult;
 import com.jzy.aicodeplatform.model.enums.CodeGenTypeEnum;
@@ -77,6 +78,20 @@ public class WorkflowContext implements Serializable {
      * 质量检查结果
      */
     private QualityResult qualityResult;
+
+    /**
+     * 图片收集计划
+     */
+    private ImageCollectionPlan imageCollectionPlan;
+
+
+    /**
+     * 并发图片收集的中间结果字段
+     */
+    private List<ImageResource> contentImages;
+    private List<ImageResource> illustrations;
+    private List<ImageResource> diagrams;
+    private List<ImageResource> logos;
 
 
     @Serial
---
+++ src/test/java/com/jzy/aicodeplatform/langgraph4j/node/ImageCollectorNodeTest.java
@@ -0,0 +1,12 @@
+package com.jzy.aicodeplatform.langgraph4j.node;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ImageCollectorNodeTest {
+
+    @Test
+    void create() {
+    }
+}
